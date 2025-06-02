c0bb2d873e3ffefd7ce59db8ff68a5abf57e8a21
JosephD0310 <dangnguyenhoang8@gmail.com>
2025-06-02 23:27:45 +0700

    update UI

--- client/src/layout/components/Header/index.tsx
+++ client/src/layout/components/Header/index.tsx
@@ -1,13 +1,13 @@
-import { Link } from "react-router-dom";
-import config from "../../../config";
+import { Link } from 'react-router-dom';
+import config from '../../../config';
 
 function Header() {
     return (
-        <Link to={config.routes.home}>
-            <div className="w-full h-20 flex flex-row justify-center items-center px-10 mb-5 bg-[#00D856] shadow-xs">
+        <div className="w-full h-20 flex flex-row justify-center items-center px-10 mb-5 bg-[#00D856] shadow-xs">
+            <Link to={config.routes.home}>
                 <img className="h-20" src="/header.png" alt="" />
-            </div>
-        </Link>
+            </Link>
+        </div>
     );
 }
 
